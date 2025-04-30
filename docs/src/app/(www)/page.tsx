3d2df4aaad8d8eb8f55ad51096619f94c2a3ac7c
Lucas Roig <lucas1.roig@gmail.com>
2025-04-30 07:42:43 +0200

    redirect from home page to button

--- docs/src/app/(www)/page.tsx
+++ docs/src/app/(www)/page.tsx
@@ -1,7 +1,6 @@
+import { redirect } from "next/navigation";
+
 export default function IndexPage() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline h-[3000px]">Hello world!</h1>
-    </div>
-  );
+  redirect("/components");
+  return null;
 }
