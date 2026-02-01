3ddbcbb17feb30744685e92482bcbda0e8cb3aa5
Lucas Roig <lucas1.roig@gmail.com>
2026-02-01 19:10:30 +0100

    export input style

--- components/input/src/input.tsx
+++ components/input/src/input.tsx
@@ -1,18 +1,13 @@
 import { cn } from "@lro-ui/utils";
 import type * as React from "react";
+import { tv } from "tailwind-variants";
+
+export const inputStyle = tv({
+  base: "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+});
 
 function Input({ className, type, ref, ...props }: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      className={cn(
-        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        className,
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
+  return <input type={type} className={cn(inputStyle(), className)} ref={ref} {...props} />;
 }
 
 export { Input };
