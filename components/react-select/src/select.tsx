87e2129258fdf5440f205a8306500125e15679ef
Lucas Roig <lucas1.roig@gmail.com>
2026-07-30 14:47:22 +0200

    add creatable select

--- components/react-select/src/select.tsx
+++ components/react-select/src/select.tsx
@@ -1,3 +1,9 @@
 import ReactSelect from "react-select";
+import CreatableSelect from "react-select/creatable";
 
 export const AutoComplete = ReactSelect;
+export const CreatableAutocomplete = CreatableSelect;
+export type BasicSelectOption = {
+  label: string;
+  value: string;
+};
